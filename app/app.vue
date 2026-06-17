f4199aede1d064f74020a7cf988ab4d0a160606d
Aldiosebastiaan <aldiosebastian9@gmail.com>
2026-06-17 10:02:59 +0700

    fix: [AS] resolve conflict in app

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-<<<<<<< Updated upstream
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-=======
   <div class="min-h-screen bg-secondary font-montserrat">
     <Navbar />
->>>>>>> Stashed changes
   </div>
 </template>
